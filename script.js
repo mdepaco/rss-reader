3172ee56b9a33b82a9c1a37c5800f6c0bb3b3717
mdepaco <mdepacsa@uoc.edu>
2024-11-19 22:26:13 +0100

    modificada funcion load

--- script.js
+++ script.js
@@ -57,12 +57,15 @@ function getFeedTitle(url) {
 function loadRSS(url) {
     fetch(`https://api.rss2json.com/v1/api.json?rss_url=${encodeURIComponent(url)}`)
         .then(response => response.json())
-        .then(data => displayArticles(data.items))
+        .then(data => {
+            console.log(data);  // Verifica si los datos están llegando correctamente
+            displayArticles(data.items);
+        })
         .catch(error => {
             console.error("Error al cargar el RSS:", error);
-            alert('Error al cargar el RSS. Intenta con otro feed.');
         });
 }
+
 
 // Mostrar artículos en la interfaz
 function displayArticles(articles, feedTitle) {
